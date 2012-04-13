c19d1255c06e203396718d904b66f319060c5985
Ryan Maloney <maloney.ryan2@gmail.com>
2012-04-13 14:31:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Handicapper::Application.config.secret_token = '91bb775a604093bfecf1407cf9f1ee920d95154c74deb9ac634c25ddeb42f93e8c8df02aa7f38ca09d9e835a4f2a393736ee773ac3271242b49109df8eaa7d81'
